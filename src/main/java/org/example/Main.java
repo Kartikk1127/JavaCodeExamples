bf4e8b559e50bd88d477bac1e6a80ae4614387ef
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-12-25 21:18:04 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        /*List<Cell> list = new ArrayList<>();
+
+        list = getFlaggedCells();*/
+    }
+
+    /*public static List<Cell> getFlaggedCells() {
+        List<Cell> flaggedCells = new ArrayList<Cell>();
+        for (Cell cell : gameBoard)
+            if (cell.isFlagged())
+                flaggedCells.add(cell);
+        return flaggedCells;
+    }*/
+
+
+    /*public static Boolean valueOf(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+    }*/
+
+    /*BigInteger(int, int, Random)*/
+
+/*
+    BigInteger.probablePrime;
+*/
+
+    NutritionFacts cocaCola = new NutritionFacts(240, 8, 100, 0, 35, 27);
+
+
+
+}
